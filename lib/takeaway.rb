dc329f5f9d2839f76f6b833de1354d0f63e9f6c3
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-25 10:32:19 +0100

    Test takeaway has a list of dishes passing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,2 +1,17 @@
 class Takeaway
+  attr_reader :menu
+
+  def initialize
+    @menu = [{
+      :name => "Jacket Potato", :price => 2.00,
+    }, {
+      :name => "Baked Beans", :price => 1.00,
+    }, {
+      :name => "Chilli Beans", :price => 1.00,
+    }]
+  end
+
+  #   def view_menu
+  #     @menu
+  #   end
 end

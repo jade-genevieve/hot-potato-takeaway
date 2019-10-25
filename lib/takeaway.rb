04c09f59f1bc9de9df35001cfcede841f7c272f1
jade-genevieve <26174144+jade-genevieve@users.noreply.github.com>
2019-10-25 11:05:22 +0100

    Test dishes can be seen with prices passing

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,16 +2,14 @@ class Takeaway
   attr_reader :menu
 
   def initialize
-    @menu = [{
-      :name => "Jacket Potato", :price => 2.00,
-    }, {
-      :name => "Baked Beans", :price => 1.00,
-    }, {
-      :name => "Chilli Beans", :price => 1.00,
-    }]
+    @menu = [
+      "Jacket Potato, 2.00",
+      "Baked Beans, 1.00",
+      "Chilli Beans, 1.00",
+    ]
   end
 
-  #   def view_menu
-  #     @menu
-  #   end
+  def view_menu
+    puts @menu
+  end
 end
